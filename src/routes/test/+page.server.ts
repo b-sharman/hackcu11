f1c2c91bb6861a605e1a20b4465181afc2cf3448
b-sharman <byron.n.sharman@gmail.com>
2025-03-01 11:34:23 -0700

    grab data from the API and pass it to client

---
+++ src/routes/test/+page.server.ts
@@ -0,0 +1,11 @@
+import type { PageLoad } from './$types';
+import { VITE_API_KEY } from '$env/static/private';
+
+export const load: PageLoad = async () => {
+  const res = await fetch(
+    `https://api.congress.gov/v3/bill?api_key=${VITE_API_KEY}`
+  );
+  const json = await res.json();
+
+  return json;
+};

c8f196c00403e7a9914c0a5c99380a5df203855e
b-sharman <byron.n.sharman@gmail.com>
2025-03-02 10:01:49 -0700

    forgot to new Header component

---
+++ frontend/src/lib/header.svelte
@@ -0,0 +1,7 @@
+<script>
+let { children } = $props();
+</script>
+
+<header class="bg-linear-to-br from-indigo-800 to-fuchsia-900 py-6 px-2 flex flex-col items-center text-white">
+  {@render children()}
+</header>

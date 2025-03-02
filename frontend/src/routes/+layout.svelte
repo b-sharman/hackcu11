5ca69aebb5ddbf489534521103a0b09e20ab5967
b-sharman <byron.n.sharman@gmail.com>
2025-03-02 09:56:41 -0700

    try no white strip at the top
    maybe gradient should be less subtle?

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -4,10 +4,4 @@ import '../app.css';
 let { children } = $props();
 </script>
 
-<header class="h-16 flex flex-row overflow-hidden text-center">
-  <h2 class="block size-full flex items-center justify-center">
-    <a href="/" class="px-4 text-lg"><span class="font-bold">Bill</span>board</a>
-  </h2>
-</header>
-
 {@render children()}

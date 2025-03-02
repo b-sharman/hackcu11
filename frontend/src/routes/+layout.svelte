96213d4a2599fe1228ee90309297a2551ecff98b
b-sharman <byron.n.sharman@gmail.com>
2025-03-02 06:25:21 -0700

    functional home button

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -15,9 +15,9 @@ let { children } = $props();
         <path fill-rule="evenodd" d="M7.72 12.53a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 1 1 1.06 1.06L9.31 12l6.97 6.97a.75.75 0 1 1-1.06 1.06l-7.5-7.5Z" clip-rule="evenodd" />
       </svg>
     </button>
-    <div class="size-full flex items-center">
+    <a href="/" class="block size-full flex items-center">
       <h2 class="w-full text-lg"><span class="font-bold">Bill</span>board</h2>
-    </div>
+    </a>
     <div class="aspect-square"></div>
   </header>
 </div>

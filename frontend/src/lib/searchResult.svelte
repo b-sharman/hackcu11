b304416e406719d9cb33b9f0a87d7f5eee5a3bb6
Kylo33 <56988649+Kylo33@users.noreply.github.com>
2025-03-01 22:37:01 -0700

    Extract ExternalUrl into its own component

--- frontend/src/lib/searchResult.svelte
+++ frontend/src/lib/searchResult.svelte
@@ -1,5 +1,6 @@
 <script lang='ts'>
 import { slide } from "svelte/transition";
+  import ExternalUrl from "./externalUrl.svelte";
 
 let { result, searchText } = $props();
 
@@ -47,12 +48,7 @@ async function expand() {
 
         <div class="flex flex-row mt-2 gap-8">
           <div class="grow">
-            <a class="text-sm text-blue-600 underline" target="_blank" href={result.url}>
-              {result.url}
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline size-3 align-middle">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
-              </svg>
-            </a>
+            <ExternalUrl url={result.url} />
           </div>
           <div>
             <a class="mr-4 text-sm text-blue-600 underline" href="/bill/{result.id}">Learn more about this bill</a>

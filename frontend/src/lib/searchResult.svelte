25cc4f5a309285da471814808900595e86f0dfd3
b-sharman <byron.n.sharman@gmail.com>
2025-03-01 21:18:56 -0700

    link to bill/[id] route

--- frontend/src/lib/searchResult.svelte
+++ frontend/src/lib/searchResult.svelte
@@ -44,13 +44,19 @@ async function expand() {
     {#if expanded}
       <div class="mt-4" in:slide out:slide>
         {@html summary}
-        <div class="mt-2">
-          <a class="text-sm text-blue-600 underline" target="_blank" href={result.url}>
-            {result.url}
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline size-3 align-middle">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
-            </svg>
-          </a>
+
+        <div class="flex flex-row mt-2 gap-8">
+          <div class="grow">
+            <a class="text-sm text-blue-600 underline" target="_blank" href={result.url}>
+              {result.url}
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline size-3 align-middle">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+              </svg>
+            </a>
+          </div>
+          <div>
+            <a class="mr-4 text-sm text-blue-600 underline" href="/bill/{result.id}">Learn more about this bill</a>
+          </div>
         </div>
       </div>
     {/if}
